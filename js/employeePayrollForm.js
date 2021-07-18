b517fc43ed7c4a910200a61fbfaddeff78aa9270
sanobar28 <sanobar.mujawar@gmail.com>
2021-07-18 19:11:29 +0530

    [Sanobar]added: reset function to reset form

--- js/employeePayrollForm.js
+++ js/employeePayrollForm.js
@@ -89,3 +89,32 @@ const getInputValueById = (id) => {
     let value = document.querySelector(id).value;
     return value;
 }
+
+//Resut form fuction to reset values
+const resetForm = () => {
+    setValue('#name', ' ');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary', ' ');
+    setValue('#notes', ' ');
+    setValue('#day', '1');
+    setValue('#month', 'January');
+    setValue('#year', '2020');
+}
+
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
+
+const setValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.value = value;
+}
